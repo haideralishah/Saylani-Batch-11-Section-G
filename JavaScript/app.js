482cc98dd6fc888d8711c1806434ca90d6bcc7c2
haideralishah <headeralishah@gmail.com>
2024-02-26 18:01:33 +0500

    JS started

---
+++ JavaScript/app.js
@@ -0,0 +1,14 @@
+/*
+String "" ''
+
+*/
+// alert('Welcome to JS world!');
+
+console.log("Welcome to JS world!");
+
+console.log("2 + 2");
+console.log(2 + 2);
+console.log(2 - 1);
+console.log(5 * 7);
+console.log(70 / 7);
+console.log(3 ** 3);
