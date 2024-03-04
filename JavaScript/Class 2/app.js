bffef3fd6a428d3afe63e2e715d25bcaf2b1cdd4
haideralishah <headeralishah@gmail.com>
2024-03-04 17:59:07 +0500

    prompt learnt

---
+++ JavaScript/Class 2/app.js
@@ -0,0 +1,172 @@
+// alert("Hello World");
+// console.log(1 + 1);
+// document.write("<h1>Hello World</h1>");
+// document.write("<p>I am the <span class='special'>master</span> of my own fate.</p>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userName = 'Aslam';
+// console.log('Welcome ' + userName + " to our class.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(2 ** 4); // +-*/  **  %
+
+
+
+// console.log(31 % 5);
+// console.log(35 % 5);
+
+
+
+
+// let num1 = 3461327;
+// console.log(num1 % 5);
+
+
+
+
+
+// let userName = prompt("what is your name?");
+// console.log("Welcome " + userName);
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = +prompt("enter a number");
+// let num2 = +prompt("enter another number");
+// console.log(num1 + num2);
+
+
+
+
+
+
+
+// let userName = prompt("What is your name?");
+// let num1 = +prompt('enter a number');
+// let num2 = +prompt('enter another number');
+
+// document.write("Welcome " + userName + "." + "<br />Your total is: " + (num1 + num2));
+
+
+
+
+
+
+
+
+// let workOutDay = 1;
+
+// console.log(workOutDay++); //1
+// console.log(++workOutDay); // 3
+// console.log(++workOutDay); //4
+
+
+
+
+
+
+
+
+
+
+
+
+// let bullets = 11;
+// console.log(bullets--);//11
+// console.log(--bullets);//9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
