ba7dfbc8d0a076619b46652b2fc41e714c6f1f12
haideralishah <headeralishah@gmail.com>
2024-03-06 18:01:31 +0500

    prompt, string number excercies done

---
+++ JavaScript/lab/lab 1/app.js
@@ -0,0 +1,122 @@
+// alert("Welcome");
+// console.log(5 + 5 - 2 * 5);
+
+// alert("03413542800");
+// console.log(03413542800);
+
+// document.write("Welcome Haider");
+// document.write("Goodbye Haider");
+
+
+
+// alert("welcome to JS land \nHappy coding");
+// console.log("welcome to JS land \nHappy coding");
+
+// document.write('Welcome to JS land <br /><h3> Happy Coding</h3>');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userName;
+// userName = "Abdullah";
+
+// console.log(userName);
+// alert(userName);
+// document.write(userName);
+// userName = "haider";
+
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userName, fatherName, rollNumber;
+// userName = "Haider";
+// fatherName = 'Shah';
+// rollNumber = "2643";
+// console.log(userName, fatherName, rollNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userName;
+// userName = "haider";
+
+// let userName = "haider";
+// let fatherName = 'Shah';
+// console.log(userName);
+
+
+// let userName = 'haider', fatherName = 'shah';
+
+
+
+
+
+
+
+
+// let userName = "haider";
+// console.log('Welcome' + " " + userName);
+
+// let userName = "Anas";
+// let greeting = "Eid Mubarak";
+// console.log(greeting + " " + userName);
+
+
+
+
---
+++ JavaScript/lab/lab 2/app.js
@@ -0,0 +1,99 @@
+// let bYear = +prompt("enter your birth year");
+// document.write("Your birth year is " + bYear);
+
+// let num1 = +prompt("enter a number");
+// let num2 = +prompt("enter another number");
+
+// console.log('Your total is ' + (num1 + num2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let jobTitle = prompt('your desired job title');
+// let country = prompt('where do you want to work?');
+// let partnerName = prompt("who do you want to marry?");
+// let kidsCount = prompt("prefered children count");
+
+// let fortune = "You will be a " + jobTitle + " in " + country + ", and married to " + partnerName + " with " + kidsCount + " kids.";
+
+// alert(fortune);
+// console.log(fortune);
+// document.write(fortune);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let emailId = prompt("what is your emai ID?");
+// console.log('My email id is ' + emailId);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let str = 5;
+// let num = "5";
+
+// console.log(typeof str);
+// console.log(typeof num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "'Haider' ordered '3' 'tshirts' from abc store of '500' each. His total bill is '1500'"
+
+
+
+
+
+let product = prompt('enter your cart item');
+let buyerName = prompt('enter your name');
+let price = prompt('enter unit price');
+let qty = prompt('enter quantity');
+let message = buyerName + " ordered " + qty + product + " from abc store of " + price + " each. His total bill is " + (qty * price) + ".";
+
+document.write(message);
+
+
+
+
+
+
