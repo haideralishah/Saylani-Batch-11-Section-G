bd30114649df75af8956ea98145a3f145afcc03e
haideralishah <headeralishah@gmail.com>
2024-03-11 17:56:52 +0500

    conditions

---
+++ JavaScript/Class 4/app.js
@@ -0,0 +1,152 @@
+// let isFarhanShopOpen = confirm('is Farhan shop open?');
+
+// if (isFarhanShopOpen) {
+//     console.log('bring french fries');
+// }
+// else {
+//     console.log('bring something to eat');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let shopName = prompt('which shop is open?');
+
+// if (shopName === "farhan") {
+//     console.log('bring french fries');
+// }
+// else if (shopName === 'almadina') {
+//     console.log('mje ande wala burger');
+// }
+// else if (shopName === 'ejaaz') {
+//     console.log('bring 1 biryani');
+// }
+// else {
+//     console.log('bring something to eat');
+// }
+
+
+
+
+
+
+
+// 40 >= cardio
+
+// 30 >= strength
+
+// 20 >= cardio + strength
+
+// 20 < crossfit
+
+// let bodyFat = +prompt('what is your body fat%?');
+
+// if (bodyFat >= 40) {
+//     console.log("You are recommended for cardio.");
+// }
+// else if (bodyFat >= 30) {
+//     console.log("You are recommended for strength training.");
+// }
+// else if (bodyFat >= 20) {
+//     console.log("You are recommended for strength + cardio training");
+// }
+// else if (bodyFat < 20 && bodyFat > 0) {
+//     console.log('you are recommended for crossfit');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let isMatriculate = confirm("Are you Matriculate");
+// let score = +prompt('what was your entry test score?')
+
+// if (isMatriculate && score >= 60) {
+//     console.log('congrats! you have got admission');
+// }
+
+
+
+
+// weight >= 300
+// time < 6
+// gender male
+// age 30+
+
+
+
+// let weight = +prompt('your weight');
+// let runningTime = +prompt('your time');
+// let gender = prompt('your gender');
+// let age = +prompt('your age');
+
+// if (
+//     weight >= 300 &&
+//     runningTime < 6 &&
+//     gender === "male" &&
+//     age > 30
+// ) {
+//     console.log('you are eligible for our fitness club.');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let availableFood = prompt('which food is available?');
+if (
+    availableFood === 'french fries' || availableFood === "burger"
+) {
+    console.log('bring 1');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
