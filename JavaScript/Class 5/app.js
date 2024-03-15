f1270be1047fbe5f893acd6230351f2bd043b849
haideralishah <headeralishah@gmail.com>
2024-03-15 16:32:16 +0500

    array introduced

---
+++ JavaScript/Class 5/app.js
@@ -0,0 +1,135 @@
+// question 15, sheet 12,13
+
+// let hour = prompt('please enter current hour');
+// if (hour >= 6 && hour <= 9) {
+//     console.log('breakfast is ready');
+// }
+// else if (hour >= 11 && hour <= 13) {
+//     console.log('lunch is ready');
+// }
+// else if (hour >= 17 && hour <= 20) {
+//     console.log('dinner is served');
+// }
+// else {
+//     console.log('please wait.');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 17, sheet 12,13
+
+// let chr = prompt('enter a character');
+
+// if (
+//     chr === "a" ||
+//     chr === "e" ||
+//     chr === "i" ||
+//     chr === "o" ||
+//     chr === "u"
+// ) {
+//     console.log('your character is a vowel');
+// }
+
+
+
+
+
+
+
+
+
+
+// let areYouGraduate = confirm('are you graduate?');
+
+// if (areYouGraduate) {
+//     let faculty = prompt('which faculty you were enrolled in?');
+//     if (faculty === "engineer") {
+//         console.log('You are appointed as Engineer.');
+//     }
+//     else if (faculty === "medical") {
+//         console.log('You are appointed as Doctor.');
+//     }
+//     else if (faculty === "commerce") {
+//         let interest = prompt('are you interested in HR or accounts?');
+//         if (interest === "HR") {
+//             console.log('You are appointed as HR assistant.');
+//         }
+//         else if (interest === "accounts") {
+//             console.log('You are appointed as accountant.');
+//         }
+//     }
+// }
+// else {
+//     console.log('You are appointed as peon.');
+// }
+
+
+
+
+
+
+
+// let students = ["Umair", "Anas", "Shoaib", "Rizwan"];
+
+// console.log(students[2]);
+// console.log(students[1]);
+
+
+
+// let students = [];
+// console.log(students);
+// students[0] = "Ahmed";
+// students[1] = "Umair";
+// students[5] = "aslam";
+
+// console.log(students[3]);
+
+
+
+
+
+
+let fruits = ['apple', 'banana', 'orange'];
+
+console.log(fruits.length);
+console.log(fruits.indexOf("watermelon"));
+console.log(fruits.includes("watermelon"));
+
+// Array.length
+// Array.indexOf()
+// Array.includes()
+
+// let vowels = ['a', 'e', 'i', 'o', 'u', "A", "E", "I", "O", "U"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
