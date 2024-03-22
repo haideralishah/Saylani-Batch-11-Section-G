2f1e3e4aa65ae2e7b497da2948b2c20691523d46
haideralishah <headeralishah@gmail.com>
2024-03-22 16:31:12 +0500

    string array methods

---
+++ JavaScript/Class 7/app.js
@@ -0,0 +1,110 @@
+// let primeNums = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23];
+
+// Array.splice(startingIndex, delete item count, add new item(s))
+// primeNums.splice(2, 3, 29, 31, 37);
+
+// console.log(primeNums);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let privNum = num.slice(3, 7);
+
+// console.log(privNum, num);
+
+
+
+
+// let cricketers = ["afridi", "misbah", "shoaib akhtar", "Muhammad Asif", "Dhoni", "Yovraj", "tendulkar", "sehwag", 'Saeed Anwar', "Amir Sohail"];
+
+// let favCricket = cricketers.slice(2, 4);
+// console.log(favCricket);
+
+
+
+
+
+
+
+
+
+
+
+
+//madam
+//civic
+// level
+// noon
+// mom
+// eye
+// let strArr = ["h", "a", "i", "d", "e", "r"];
+// console.log(strArr.reverse().join(""));
+// console.log(strArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let str = 'haider ali shah';
+
+// console.log(str[6]);
+
+// console.log(str.length);
+
+// console.log(str.slice(7, 10));
+
+
+
+
+
+
+
+
+
+
+
+
+// let firstName = 'hAIDeR ali';
+
+// console.log(firstName.toLowerCase());
+// console.log(firstName.toUpperCase());
+// let userName = prompt('what is your name?');
+
+// if (firstName.toLowerCase() === userName.toLowerCase()) {
+
+// }
+
+
+
+let firstName = "hAIdEr"; // Haider
+
+
+
+
+
