fe5dd35db25ef0582b3daa18d3ce82e622a5e90a
haideralishah <headeralishah@gmail.com>
2024-03-27 13:33:28 +0500

    loop

---
+++ JavaScript/Class 8/app.js
@@ -0,0 +1,186 @@
+// let fullName = ["haider", 'ali', 'shah'];
+
+// let myName = fullName.join("-");
+
+// console.log(myName);
+
+
+
+// let myName = "haider ali shah";
+// let myNameArr = myName.split(" ");
+
+// console.log(myNameArr);
+
+
+
+
+
+// let word = prompt('find if palindrom or not?');
+// let revWord = word.split("").reverse().join("");
+
+// if (word === revWord) {
+//     console.log("yes, it is a palindrome");
+// }
+// else {
+//     console.log('no it is not a palindrom');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+starting point
+ending point
+changes
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i <= 50; i++) {
+//     console.log(i);
+// }
+
+// console.log('hello world');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 100; i > 0; i--) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i = i + 4) {
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let table = +prompt('enter table name');
+
+// for (let i = 1; i <= 200; i++) {
+//     // 2 x 1 = 2
+//     // 2 x 2 = 4
+//     document.write("<h3>" + table + " x " + i + " = " + table * i + "</h3>")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let students = ["aslam", "akram", "muzammil", "abdullah", "ahmed", "abdul karim", "anas"];
+
+for (let i = 0; i < students.length; i++) {
+    console.log("Hello " + students[i]);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
