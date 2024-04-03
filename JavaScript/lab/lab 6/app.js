3f611ef859f827603c506f22ff216e75cce9772a
haideralishah <headeralishah@gmail.com>
2024-04-03 16:26:22 +0500

    Math methods

---
+++ JavaScript/lab/lab 6/app.js
@@ -0,0 +1,218 @@
+// haider HAIDER hAIdEr  -->   Haider
+// let userName = prompt('what is your name?');
+// // haider
+// let firstChr = userName[0];
+// firstChr = firstChr.toUpperCase();
+
+// let restChr = userName.slice(1);
+// restChr = restChr.toLowerCase();
+
+// console.log(firstChr + restChr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ['banana','apple', 'watermelon', 'orange'];
+
+// fruits.sort();
+
+// console.log(fruits);
+
+
+
+
+
+
+
+// let fruits = ['banana', 'apple', 'watermelon', 'orange'];
+
+// let sortedFruits = fruits.toSorted();
+// let reversedFruits = fruits.toReversed();
+
+// console.log(fruits, sortedFruits, reversedFruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = 2.9;
+// let round = Math.round(num); // 3
+// let ceiledVal = Math.ceil(num); // 3
+// let floorVal = Math.floor(num); //2
+
+// console.log(round, ceiledVal, floorVal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(Math.max(5, 7, 3, 7, 40, 10571, 56, 741, 62, 450));
+
+// console.log(Math.min(5, 7, 3, 7, 40, 3, 56, 741, 62, 450));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = [1, 2, 3];
+// let num2 = [4, 5, 6];
+
+// let num3 = [...num1, ...num2];
+// // [1,2,3,4,5,6]
+
+// console.log(num3);
+// console.log(Math.max(...num3));
+// console.log(Math.min(...num3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let random = Math.ceil(Math.random() * 6);
+
+console.log(random);
+
+
