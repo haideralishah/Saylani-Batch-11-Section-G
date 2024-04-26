97d9a98b53038103034bb78a52f0b1054f4c8f59
haideralishah <headeralishah@gmail.com>
2024-04-26 18:01:52 +0500

    functions concept

--- JavaScript/Class 12 Date and time/app.js
+++ JavaScript/Class 12 Date and time/app.js
@@ -53,9 +53,135 @@
 
 
 
-let dob = new Date("October 4, 1991");
-let now = new Date();
+// let dob = new Date("October 4, 1991");
+// let now = new Date();
 
-let age = now.getTime() - dob.getTime()
+// let age = now.getTime() - dob.getTime()
 
-console.log(age / 1000 / 60 / 60 / 24 / 365.25);
+// console.log(age / 1000 / 60 / 60 / 24 / 365.25);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addDashOnOddNum(str) {
+//     if (!isNaN(str)) {
+//         let newStr = "";
+
+//         for (let i = 0; i < str.length; i++) {
+//             if (
+//                 str[i] % 2 !== 0 &&
+//                 str[i + 1] % 2 !== 0 &&
+//                 !isNaN(str[i + 1] % 2)
+//             ) {
+//                 newStr += str[i] + "-";
+//             }
+//             else {
+//                 newStr += str[i];
+//             }
+//         }
+//         console.log(newStr);
+
+//     }
+//     else {
+//         console.log('Number required');
+//     }
+// }
+
+// addDashOnOddNum("23497412579");
+// addDashOnOddNum("5796213");
+// addDashOnOddNum("588a5288");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let today = new Date();
+// let startDate = new Date("December 11, 2023");
+
+// let diff = today - startDate;
+
+// console.log(diff / 1000 / 60 / 60 / 24 / 30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dob = new Date("October 20, 2024");
+// console.log(dob);
+
+// dob.setDate(4);
+
+// console.log(dob);
+
+// dob.setFullYear(2025);
+
+// console.log(dob);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/Class 13 Functions/app.js
@@ -0,0 +1,191 @@
+// function cook() {
+//     console.log('bring chai paratha');
+// }
+
+// cook();
+// cook();
+// cook();
+// cook();
+// cook();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function cook(item1, item2) {
+//     console.log("Bring " + item1 + " " + item2);
+// }
+
+// cook("Biryani", "raita");
+// cook("bengan", "tende");
+
+
+
+
+
+
+
+
+
+
+
+// function findCube(num) {
+//     console.log(num * num * num);
+// }
+
+// findCube(3);
+
+// findCube(9);
+
+// findCube(5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function displayMessage(userName, msg = 'How may I help you?') {
+//     console.log(userName + " " + msg);
+// }
+
+// displayMessage("abdullah", "Assalamualaikum");
+
+// displayMessage("Muzammil", "apni fees submit krwayen");
+
+// displayMessage("Ahmed");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function makeFruitChaat() {
+//     for (let i = 0; i < arguments.length; i++) {
+//         console.log("crush " + arguments[i]);
+
+//     }
+// }
+
+// makeFruitChaat("banana", "apple", "orange");
+// makeFruitChaat("melon");
+// makeFruitChaat("mango", "grapes");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function letMeKnowYourWishes() {
+//     console.log(arguments);
+// }
+
+// letMeKnowYourWishes("no traffic", "dinner biryani", "petrol 50Liter");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert();
+// let userName = prompt('what is your name?');
+
+
+
+
+
+
+
+
+function add2Num(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+let findSum = add2Num(2, 6);
+
+console.log(findSum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
