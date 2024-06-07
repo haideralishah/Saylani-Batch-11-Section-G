9696221b06b5219dafc4d739989e3dd0416012ea
haideralishah <headeralishah@gmail.com>
2024-06-07 17:41:02 +0500

    es 6

--- JavaScript/Class 22 Advance JS/app.js
+++ JavaScript/Class 22 Advance JS/app.js
@@ -190,6 +190,27 @@ array, objects, functions
 
 
 
+// /* Promise */
+// let treat = new Promise(function (resolve, reject) {
+//     let isNotesAvail = confirm("Have you arranged English Notes?");
+//     if (isNotesAvail) {
+//         let foodWish = prompt('what do you want to eat?');
+//         resolve(foodWish);
+//     }
+//     else {
+//         reject("dosti khtm");
+//     }
+// });
+
+
+
+// treat
+//     .then(function (foodItem) {
+//         console.log(`Thanks bro, ${foodItem} is due on me.`);
+//     })
+//     .catch(function (e) {
+//         console.log('Tm se ye umeed ni thi.', e);
+//     })
 
 
 
@@ -202,24 +223,298 @@ array, objects, functions
 
 
 
-/* Promise */
-let treat = new Promise(function (resolve, reject) {
-    let isNotesAvail = confirm("Have you arranged English Notes?");
-    if (isNotesAvail) {
-        let foodWish = prompt('what do you want to eat?');
-        resolve(foodWish);
-    }
-    else {
-        reject("dosti khtm");
-    }
-});
 
 
 
-treat
-    .then(function (foodItem) {
-        console.log(`Thanks bro, ${foodItem} is due on me.`);
-    })
-    .catch(function (e) {
-        console.log('Tm se ye umeed ni thi.', e);
-    })
+
+
+
+
+
+
+
+
+
+
+
+// let user = {
+//     userName: 'Haider Ali Shah',
+//     rollNumber: 2643,
+//     isTeacher: true,
+//     subjects: ['JavaScript'],
+//     fathersName: "Muhammad Shah Alam"
+// }
+
+// function printStudent(student) {
+//     let userName = 'Abdullah';
+
+//     const {
+//         userName: studentName,
+//         fathersName,
+//         rollNumber,
+//         ...remainingProps
+//     } = student;
+
+//     console.log(remainingProps)
+
+//     let str = `
+//         Welcome ${studentName} son of ${fathersName}, your roll number is ${rollNumber}.
+//     `;
+
+//     document.write(str);
+
+// }
+
+// printStudent(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let car = {
+//     carName: 'Mehran',
+//     varient: "VXR",
+//     model: 2005,
+//     features: {
+//         isAirConditioned: true,
+//         isAutomatic: false,
+//         fuelAverage: "best",
+//         easyToDhakka: true
+//     }
+// }
+// const {
+//     carName,
+//     varient,
+//     features: { fuelAverage, easyToDhakka }
+// } = car;
+
+// console.log(carName, varient, fuelAverage, easyToDhakka);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ['apple', 'banana', 'watermelon', 'orange', 'mango'];
+
+// const [fruit1, , , fruit2] = fruits;
+
+// console.log(fruit1, fruit2);
+
+// const [fruit1, , fruit2, , fruit3] = fruits;
+
+// console.log(fruit1, fruit2, fruit3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Arrow Function
+
+() => {
+    console.log('hello');
+}
+
+(userName, rollNumber) => {
+    console.log(`Welcome ${userName}, your roll number is ${rollNumber}`);
+}
+
+
+() => `Welcome.`;
+
+(userName, rollNumber) => `Welcome ${userName}, your roll number is ${rollNumber}`;
+
+userName => `Welcome ${userName}`
+
+*/
+
+
+
+// function greet() {
+//     console.log('hello');
+// }
+
+// () => {
+//     console.log('hello');
+// }
+
+// setTimeout(() => {
+//     console.log('hello');
+// }, 500);
+
+
+
+// const greet = () => {
+//     console.log('hello world');
+// }
+
+// greet();
+
+
+
+
+
+
+
+// const greet = (userName, rollNumber) => {
+//     console.log(`Welcome ${userName}, your roll number is ${rollNumber}`);
+// }
+
+// greet('haider', 2435);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const makeMessage = (userName, rollNumber) => `Welcome ${userName}, your roll number is ${rollNumber}`;
+
+// let greet = makeMessage("Abdullah", 2643);
+
+// console.log(greet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const makeMessage = userName => `Welcome ${userName}`;
+
+let greet = makeMessage("Abdullah");
+
+console.log(greet);
+
+
+
+
