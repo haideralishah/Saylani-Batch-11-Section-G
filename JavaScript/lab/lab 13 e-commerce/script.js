6aff72e4e5fb4f7217521bf31cdedafc5d9a1a3a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-06-12 18:11:02 +0500

    add to cart feature implemented

--- JavaScript/lab/lab 13 e-commerce/script.js
+++ JavaScript/lab/lab 13 e-commerce/script.js
@@ -1,5 +1,5 @@
 const slideImages = [
-  "https://burgerlab.com.pk/wp-content/uploads/2024/05/app-banner-2.jpg?c062ef&c062ef",
+  "https://pixexid.com/api/download/image/a-burger-with-a-sense-of-wanderlust-backpacks-through-a-food-landscape-with-fry-9hbp0rfh.jpeg",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn6q8_0SwnNkM0c0vOT_twL1yzrcvlGuToSw&s",
   "https://pixexid.com/api/download/image/a-burger-with-a-sense-of-wanderlust-backpacks-through-a-food-landscape-with-fry-9hbp0rfh.jpeg",
 ];
@@ -17,38 +17,28 @@ displaySlider();
 
 const products = [
   {
-    image:
-      "https://burgerlab.com.pk/wp-content/uploads/2022/01/doppler.png?c062ef&c062ef",
+    id: "502c305a-997e-4534-97b8-f6f7862f3737",
+    image: "https://em-cdn.eatmubarak.pk/55018/dish_image/1693985158.jpg",
     productName: "THE Doppler",
     price: "$10.00",
   },
   {
-    image:
-      "https://burgerlab.com.pk/wp-content/uploads/2022/01/quadra.png?c062ef&c062ef",
+    id: "67aadbfe-a6d9-4d8d-a436-27c369cff5ff",
+    image: "https://em-cdn.eatmubarak.pk/55018/dish_image/1693985158.jpg",
     productName: "Pentra RELOADED",
     price: "$20.00",
   },
   {
-    image:
-      "https://burgerlab.com.pk/wp-content/uploads/2022/01/all-american-4.png?c062ef&c062ef",
+    id: "54c3c17e-b3e9-469b-800a-b065d193a6d9",
+    image: "https://em-cdn.eatmubarak.pk/55018/dish_image/1693985158.jpg",
     productName: "ALL PAKISTANI DOUBLE CHEESE",
     price: "$50.00",
   },
   {
+    id: "e2085e01-f142-4475-8124-e24722cff1d6",
     image: "https://em-cdn.eatmubarak.pk/55018/dish_image/1693985158.jpg",
     productName: "Big Bang",
     price: "$40.00",
-  },
-];
-
-let cart = [
-  {
-    image:
-      "https://burgerlab.com.pk/wp-content/uploads/2022/01/doppler.png?c062ef&c062ef",
-    productName: "THE Doppler",
-    price: "$10.00",
-    qty: 3,
-    totalPrice: this.price * this.qty,
   },
 ];
 
@@ -65,7 +55,7 @@ function createProductCard() {
           />
           <h3>${product.productName}</h3>
           <p>${product.price}</p>
-          <p>Add to Cart</p>
+          <button onclick="addToCartinObj(${i})">Add to Cart</button>
         </div>
     `;
     productListContainer.innerHTML += productCard;
@@ -73,3 +63,55 @@ function createProductCard() {
 }
 
 createProductCard();
+
+let cart = {};
+
+function addToCartinObj(index) {
+  const { id } = products[index];
+
+  if (id in cart) {
+    cart[id].qty = cart[id].qty + 1;
+    cart[id].totalPrice = cart[id].qty * cart[id].price.slice(1);
+  } else {
+    cart[id] = { ...products[index] };
+    cart[id].qty = 1;
+    cart[id].totalPrice = cart[id].qty * cart[id].price.slice(1);
+  }
+  console.log(cart);
+}
+
+
+
+
+
+
+
+// let cart = [];
+// function addToCartinArr(ind) {
+//   const { id } = products[ind];
+
+//   let matchFound = false;
+//   let targetIndex;
+
+//   cart.forEach((item, index) => {
+//     if (item.id === id) {
+//       matchFound = true;
+//       targetIndex = index;
+//     }
+//   });
+
+//   if (matchFound) {
+//     const product = cart[targetIndex];
+
+//     product.qty = product.qty + 1;
+//     product.totalPrice = product.qty * product.price.slice(1);
+
+//   } else {
+//     let cloneProduct = { ...products[ind] };
+//     cloneProduct.qty = 1;
+//     cloneProduct.totalPrice = cloneProduct.qty * cloneProduct.price.slice(1);
+//     cart.push(cloneProduct);
+//   }
+
+//   console.log(cart);
+// }
