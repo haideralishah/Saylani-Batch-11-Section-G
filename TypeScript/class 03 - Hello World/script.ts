873840c8722d629aa94bfd0ac7b50c11a29d89ed
skahmerali <38328235+skahmerali@users.noreply.github.com>
2024-07-01 17:46:28 +0500

    typescript enums touples and casting

---
+++ TypeScript/class 03 - Hello World/script.ts
@@ -0,0 +1,220 @@
+
+
+
+// let basicColors2: string[];
+
+// let basicColors: [string, string, string];
+
+// basicColors = ['red', 'green', 'blue'];
+
+// let mixArr2: (string | number | boolean)[];
+// let mixArr: readonly [string, number, boolean];
+
+// mixArr = ["hello", 1, true];
+
+
+
+
+
+
+
+
+// let mixArr: readonly [string, number, boolean] = ["hello", 1, true];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const pending = "pending";
+// const confirmed = "confirmed";
+// const shipped = "shipped";
+// const delivered = "delivered";
+
+// enum OrderStatus {
+//     pending,
+//     confirmed,
+//     shipped,
+//     delivered
+// }
+
+// console.log(OrderStatus.shipped);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// enum Province {
+//     sindh = 'Sindh',
+//     punjab = "Punjab",
+//     balochistan = "Balochistan",
+//     KP = "KP",
+//     GB = "GB"
+// }
+
+// console.log(Province.balochistan)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// enum HTTPCode {
+//     success = 200, 
+//     badReqest = 400,
+//     myMistake = 500
+// }
+
+/*use with key*/
+// console.log(HTTPCode.success);
+
+/*use with value*/
+// console.log(HTTPCode[400]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// enum MathConst {
+//     PI = 3.14,
+//     ranNum = Math.random(),
+//     sum = 5 + 9
+// }
+
+// console.log(MathConst.PI);
+// console.log(MathConst.sum);
+
+// console.log(MathConst.ranNum);
+// console.log(MathConst.ranNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let a = prompt("what is your name?");
+
+let userName: string = a as string;
+
+let userName2: string = <string>a;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
