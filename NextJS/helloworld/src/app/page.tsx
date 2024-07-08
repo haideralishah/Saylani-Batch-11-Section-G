c14a22f2912e9ca37d48bc72f061fa9371e4d4ba
aiwithqasim <qasimhassan1020@gmail.com>
2024-07-08 18:18:41 +0500

    next js introduction

---
+++ NextJS/helloworld/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia totam repellat eos omnis odit eius autem dicta impedit sunt hic perspiciatis cum quasi culpa, nihil repudiandae in eveniet tempore placeat.</p>
+    </div>
+  )
+}
