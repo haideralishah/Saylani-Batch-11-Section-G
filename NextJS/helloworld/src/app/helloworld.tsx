7d03860bfdd3be937ffac6b6b1974c14cb570f3b
skahmerali <sheikhahmerali75@gmail.com>
2024-07-10 17:47:48 +0500

    hello world component used

---
+++ NextJS/helloworld/src/app/helloworld.tsx
@@ -0,0 +1,10 @@
+
+export default function HelloWorld(){
+
+    return(
+        <>
+            <h1>Hello World</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas vitae, praesentium voluptates perferendis nesciunt ad voluptatum ipsam quam quos exercitationem enim illo numquam mollitia minus reiciendis saepe cupiditate consequuntur itaque.</p>
+      </>
+    )
+}
--- NextJS/helloworld/src/app/page.tsx
+++ NextJS/helloworld/src/app/page.tsx
@@ -1,9 +1,15 @@
+import HelloWorld from "./helloworld";
 
-export default function Home() {
+
+export default function Home(){
+  
   return (
-    <div>
-      <h1>hello world</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia totam repellat eos omnis odit eius autem dicta impedit sunt hic perspiciatis cum quasi culpa, nihil repudiandae in eveniet tempore placeat.</p>
-    </div>
-  )
+    <>
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+    </>
+  );
 }
+
